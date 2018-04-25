a2a976c71caaba22a31582d47c5796d2aeaaccfc
bgayman <bgayman@apppartner.com>
2018-04-24 22:15:37 -0400

    Inital commit

--- Wall/ViewController.swift
+++ Wall/ViewController.swift
@@ -13,6 +13,8 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    let updateQueue = DispatchQueue(label: "com.bradgayman.wall.serialSceneKitQueue")
+    var planes = [Plane]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +22,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        sceneView.automaticallyUpdatesLighting = true
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -35,7 +41,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = [.horizontal, .vertical]
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -54,14 +60,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
+
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+        let anchorNode = SCNNode()
+        anchorNode.name = "anchor"
+        sceneView.scene.rootNode.addChildNode(anchorNode)
+        return anchorNode
     }
-*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let anchor = anchor as? ARPlaneAnchor,
+              let device = MTLCreateSystemDefaultDevice() else { return }
+        print("didUpdate")
+        
+        let plane = ARSCNPlaneGeometry(device: device)
+        plane?.update(from: anchor.geometry)
+        node.geometry = plane
+        if anchor.alignment == .horizontal {
+            node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue.withAlphaComponent(0.3)
+        } else {
+            node.geometry?.firstMaterial?.diffuse.contents = UIColor.green.withAlphaComponent(0.3)
+        }
+        
+    }
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -76,5 +99,81 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+}
+
+final class Plane: SCNNode {
+    let anchor: ARPlaneAnchor
+    private let planeGeometry: SCNPlane
+    
+    init(anchor: ARPlaneAnchor) {
+        self.anchor = anchor
+        self.planeGeometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.green.withAlphaComponent(0.3)
+        material.isDoubleSided = true
+        self.planeGeometry.firstMaterial = material
+        let planeNode = SCNNode(geometry: self.planeGeometry)
+        planeNode.position = SCNVector3(anchor.center.x, 0.0, anchor.center.y)
+        super.init()
+        self.addChildNode(planeNode)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi * 0.5, 1.0, 0.0, 0.0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func update(anchor: ARPlaneAnchor) {
+        self.planeGeometry.width = CGFloat(anchor.extent.x)
+        self.planeGeometry.height = CGFloat(anchor.extent.z)
+        self.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+    }
+}
+
+extension float4x4 {
+    /**
+     Treats matrix as a (right-hand column-major convention) transform matrix
+     and factors out the translation component of the transform.
+     */
+    var translation: float3 {
+        get {
+            let translation = columns.3
+            return float3(translation.x, translation.y, translation.z)
+        }
+        set(newValue) {
+            columns.3 = float4(newValue.x, newValue.y, newValue.z, columns.3.w)
+        }
+    }
+    
+    /**
+     Factors out the orientation component of the transform.
+     */
+    var orientation: simd_quatf {
+        return simd_quaternion(self)
+    }
+    
+    /**
+     Creates a transform matrix with a uniform scale factor in all directions.
+     */
+    init(uniformScale scale: Float) {
+        self = matrix_identity_float4x4
+        columns.0.x = scale
+        columns.1.y = scale
+        columns.2.z = scale
+    }
+}
+
+// MARK: - CGPoint extensions
+
+extension CGPoint {
+    /// Extracts the screen space point from a vector returned by SCNView.projectPoint(_:).
+    init(_ vector: SCNVector3) {
+        self.init(x: CGFloat(vector.x), y: CGFloat(vector.y))
+    }
+    
+    /// Returns the length of a point when considered as a vector. (Used with gesture recognizers.)
+    var length: CGFloat {
+        return sqrt(x * x + y * y)
     }
 }
